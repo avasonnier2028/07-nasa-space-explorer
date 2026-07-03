3e415cf94a4c7c6ceb69c06d1b0c94547290c87a
Ava Sonnier <ava.sonnier2028@tamu.edu>
2026-07-03 17:48:54 +0000

    Added api request url

--- js/script.js
+++ js/script.js
@@ -7,3 +7,6 @@ const endInput = document.getElementById('endDate');
 // - Default to a range of 9 days (from 9 days ago to today)
 // - Restrict dates to NASA's image archive (starting from 1995)
 setupDateInputs(startInput, endInput);
+
+//Custom date range
+//https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=1995-06-16&end_date=2025-07-03
